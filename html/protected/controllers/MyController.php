ef01015ec722e20a29db594c0e0a35f11e04a26d
ptflp <filippov.pa@s-vfu.ru>
2017-10-30 22:18:13 +0900

    myController 2 sub controller tags

--- html/protected/controllers/MyController.php
+++ html/protected/controllers/MyController.php
@@ -8,9 +8,8 @@ class MyController extends Controller
 	public function actionOne()
 	{
 		echo '111';
-		echo $this->one();
 	}
-	private function one() {
-		return 'one';
+	public function actionTwo() {
+		echo 'one';
 	}
 }
