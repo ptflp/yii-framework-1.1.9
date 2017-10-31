ce10070236e5611947c5cccd0c9e20397772a554
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 21:47:16 +0900

    CHtml bunch on controller path/somepath dynamicaly

--- html/protected/views/my/one.php
+++ html/protected/views/my/one.php
@@ -6,5 +6,6 @@ foreach ($models as $model) {
 		array('page/view', 'id'=>$model->id),
 		array('target'=>'_blank','id'=>$model->id, 'sfsfs'=>'afsf')
 	);
+	echo CHtml::normalizeUrl(array('page/view', 'id'=>$model->id));
 	echo '<hr/>';
 }
