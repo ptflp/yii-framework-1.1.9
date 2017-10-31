5103b1544aeddcef224ba33f91bf0bd039f1fb19
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 10:06:13 +0900

    customer generated

--- html/protected/config/main.php
+++ html/protected/config/main.php
@@ -37,7 +37,7 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -46,7 +46,7 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
 
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
