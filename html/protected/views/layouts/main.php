afe7d59cd018ab646c5f25a38a3f08c4ea1f6e8e
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 23:10:44 +0900

    CMenu added class

--- html/protected/views/layouts/main.php
+++ html/protected/views/layouts/main.php
@@ -30,14 +30,13 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index'), 'items'=>array(
-					array('label'=> 'Some label', 'url' => '/my')
-				)),
+				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
+			'htmlOptions' => array('class'=> 'fasfasfa')
 		)); ?>
 	</div><!-- mainmenu -->
 	<?php if(isset($this->breadcrumbs)):?>
