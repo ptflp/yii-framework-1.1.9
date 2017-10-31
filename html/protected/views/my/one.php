18cf07e13a4412da11ccd7881b10aa760936c79f
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 21:41:25 +0900

    adding attributes

--- html/protected/views/my/one.php
+++ html/protected/views/my/one.php
@@ -1,6 +1,6 @@
 <?php
 foreach ($models as $model) {
 	$title = CHtml::encode($model->title);
-	echo CHtml::link($title, array('page/view', 'id'=>$model->id));
+	echo CHtml::link($title, array('page/view', 'id'=>$model->id),array('target'=>'_blank','id'=>$model->id, 'sfsfs'=>'afsf'));
 	echo '<hr/>';
 }
