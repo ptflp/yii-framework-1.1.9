204b8b4d837d41738766f1f90ba6b019434b6a76
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 17:05:44 +0900

    custom error message for form validator

--- html/protected/models/Page.php
+++ html/protected/models/Page.php
@@ -28,7 +28,7 @@ class Page extends CActiveRecord
 		return array(
 			array('title, text', 'required'),
 			array('title', 'length', 'max'=>255),
-			array('title', 'compare', 'compareAttribute'=>'text'),
+			array('title', 'compare', 'compareAttribute'=>'text', 'message'=>'Шел нахуй'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
 			array('id, title, text', 'safe', 'on'=>'search'),
