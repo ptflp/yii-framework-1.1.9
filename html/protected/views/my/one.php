382d5d21d85ca61721b3b7d4e2883fcb33a587f9
ptflp <filippov.pa@s-vfu.ru>
2017-10-31 21:42:28 +0900

    redable styling

--- html/protected/views/my/one.php
+++ html/protected/views/my/one.php
@@ -1,6 +1,10 @@
 <?php
 foreach ($models as $model) {
 	$title = CHtml::encode($model->title);
-	echo CHtml::link($title, array('page/view', 'id'=>$model->id),array('target'=>'_blank','id'=>$model->id, 'sfsfs'=>'afsf'));
+	echo CHtml::link(
+		$title, 
+		array('page/view', 'id'=>$model->id),
+		array('target'=>'_blank','id'=>$model->id, 'sfsfs'=>'afsf')
+	);
 	echo '<hr/>';
 }
