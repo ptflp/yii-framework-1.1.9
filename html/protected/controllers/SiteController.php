929a7a856fbe889aa75de97deba484e6351a5f39
ptflp <filippov.pa@s-vfu.ru>
2017-11-02 20:40:14 +0900

    removing useless rows

--- html/protected/controllers/SiteController.php
+++ html/protected/controllers/SiteController.php
@@ -28,7 +28,7 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
-		echo Str::strOut('1111');
+		//echo Str::strOut('1111');
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');
