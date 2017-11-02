ecc86f9a2763f36ce20eff49b45affef892a2a4d
ptflp <filippov.pa@s-vfu.ru>
2017-11-02 20:46:05 +0900

    category MVC generated by Gii

--- html/protected/runtime/gii-1.1.19/ModelCode.php
+++ html/protected/runtime/gii-1.1.19/ModelCode.php
@@ -5,6 +5,6 @@ return array (
   'tablePrefix' => 'o_',
   'modelPath' => 'application.models',
   'baseClass' => 'CActiveRecord',
-  'buildRelations' => '1',
+  'buildRelations' => '0',
   'commentsAsLabels' => '0',
 );
