839a301c78fa7c8a6a5d0558450b2daace60e7d4
ptflp <filippov.pa@s-vfu.ru>
2017-11-03 22:44:52 +0900

    adding email to contact form

--- html/protected/controllers/SiteController.php
+++ html/protected/controllers/SiteController.php
@@ -67,7 +67,7 @@ class SiteController extends Controller
 					"Content-Type: text/plain; charset=UTF-8";
 
 				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+				Yii::app()->user->setFlash('contact','Удачная отправка письма');
 				$this->refresh();
 			}
 		}
